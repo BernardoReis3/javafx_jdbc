490c88fbb94866125337d8acdbb67430de4b067b
Bernardo Reis <bernardomreis19@gmail.com>
2024-05-15 11:36:14 +0100

    Create Department View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("Department item clicked");
+		loadAboutView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
