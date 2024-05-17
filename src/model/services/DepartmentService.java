811ecc80e55f4b9400d7486647908550ac6e4b86
Bernardo Reis <bernardomreis19@gmail.com>
2024-05-17 23:42:44 +0100

    Add jdbc implementation

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,21 +1,24 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 
+	private DepartmentDao departmentDAO = DaoFactory.createDepartmentDao();
 	
 	public List<Department> findAll(){
+		/*
 		List<Department> listDeps = new ArrayList<>();
 		//Mock the data of list department for now
 		listDeps.add(new Department(1, "Books"));
 		listDeps.add(new Department(2, "Computers"));
 		listDeps.add(new Department(3, "Phones and gadgets"));
-		
-		return listDeps;
+		*/
+		return departmentDAO.findAll();
 		
 	}
 }
